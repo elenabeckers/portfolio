385db2ef02df86bf06ffce940fcf2088964e747b
Elena Beckers <46313514+elenabeckers@users.noreply.github.com>
2025-12-08 14:19:14 -0700

    feat(ui): update header/footer, add LinkButton, refresh theme fonts (#8)

---
+++ components/common/LinkButton.tsx
@@ -0,0 +1,31 @@
+import { AnchorHTMLAttributes } from "react";
+import clsx from "clsx";
+
+interface LinkButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  variant?: "primary" | "secondary";
+  href: string;
+}
+
+export default function LinkButton({
+  children,
+  variant = "primary",
+  className: externalClassName,
+  ...rest
+}: LinkButtonProps) {
+  const classes = clsx(
+    "inline-block cursor-pointer rounded-lg border border-foreground px-5 font-geist-mono text-sm font-bold tracking-wider transition-all duration-300",
+
+    variant === "primary" &&
+      "bg-foreground py-3.5 text-background hover:bg-background hover:text-foreground",
+
+    variant === "secondary" &&
+      "bg-transparent py-3 text-foreground hover:bg-foreground hover:text-background",
+
+    externalClassName
+  );
+  return (
+    <a className={classes} {...rest}>
+      {children}
+    </a>
+  );
+}
